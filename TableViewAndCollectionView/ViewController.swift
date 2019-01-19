59ef7e1af81dcd44e0ae1119ca8313f8c86f68f7
Vivek Rai <vivekraiiosdev@gmail.com>
2019-01-19 15:59:21 +0530

    Initial Commit

---
+++ TableViewAndCollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewAndCollectionView
+//
+//  Created by Vivek Rai on 19/01/19.
+//  Copyright © 2019 Vivek Rai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
